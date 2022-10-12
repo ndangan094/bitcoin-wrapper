e64cf5088048dc997eb0d03213401d418e54c564
Dang An <ndangan094@gnail.com>
2022-10-13 02:06:20 +0700

    fix

--- src/index1.js
+++ src/index1.js
@@ -10,17 +10,24 @@ const TronClient = ({ privateKey }) => {
     return tronWeb;
 }
 
-const test = async ({ address }) => {
+const test = async () => {
     const TronWeb = require('tronweb')
+    const {BigNumber} = require("ethers");
     const HttpProvider = TronWeb.providers.HttpProvider;
     const fullNode = new HttpProvider("https://api.nileex.io");
     const solidityNode = new HttpProvider("https://api.nileex.io");
     const eventServer = new HttpProvider("https://event.nileex.io");
     const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,"7c7f4cbdb5b3111a197c63d237da69baa0077300bde700667c067dc92c90d288");
-    let contract = await tronWeb.contract().at(address);
-    console.log(contract);
-    let result = await Promise.all([contract.symbol().call(), contract.decimals().call()]);
-    console.log({ symbol: result[0], decimals: result[1] }) ;
+    const { abi } = await tronWeb.trx.getContract("TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3");
+    const contract = tronWeb.contract(abi.entrys, "TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3");
+    const resp = await contract.methods.transfer("TLqBSejfpqWeCv1oRLTgts5u9Q5JxiiG51", BigNumber.from("1000000000000000000") ).send();
+    while(true){
+        try {
+            let a = await tronWeb.trx.getConfirmedTransaction(resp);
+            break;
+        } catch (e) {}
+    }
+    console.log(resp);
 }
 
-test({address:"TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj"})
+test()
--- staging/index.js
+++ staging/index.js
@@ -87,27 +87,40 @@ class TronJS {
         const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,privateKey);
         let contract = await tronWeb.contract().at(address);
         let result = await Promise.all([contract.symbol().call(), contract.decimals().call()]);
-        return { symbol: result[0], decimals: result[1] };
+        return { symbol: result[0], decimals: result[1].toString() };
     }
 
-
-    async transferTRC20(tokenContract, to, privateKey, amount) {
-        let tronWeb = TronClient(privateKey);
-        const { abi } = await tronWeb.trx.getContract(tokenContract);
-        const contract = tronWeb.contract(abi.entrys, tokenContract);
-        const resp = await contract.methods.transfer(to, amount).send();
-        return resp;
-    }
-
-   
-
-    async getTRC20Balance({ tokenContract, from, privateKey }) {
-        let tronWeb = TronClient(privateKey);
+    async getTRC20Balance( rpc,tokenContract, from, privateKey ) {
+        const TronWeb = require('tronweb')
+        const HttpProvider = TronWeb.providers.HttpProvider;
+        const fullNode = new HttpProvider(rpc);
+        const solidityNode = new HttpProvider(rpc);
+        const eventServer = new HttpProvider(rpc);
+        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,privateKey);
         let contract = await tronWeb.contract().at(tokenContract);
         let result = await Promise.all([contract.balanceOf(from).call(), contract.decimals().call()]);
         return { balanceInWei: result[0].toString(), balance: result[0] / Math.pow(10, result[1]) };
     }
 
+    async transferTRC20(rpc,tokenContract, to, privateKey, amount) {
+        const TronWeb = require('tronweb')
+        const {BigNumber} = require("ethers");
+        const HttpProvider = TronWeb.providers.HttpProvider;
+        const fullNode = new HttpProvider(rpc);
+        const solidityNode = new HttpProvider(rpc);
+        const eventServer = new HttpProvider(rpc);
+        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,privateKey);
+        const { abi } = await tronWeb.trx.getContract(tokenContract);
+        const contract = tronWeb.contract(abi.entrys, tokenContract);
+        const resp = await contract.methods.transfer(to, BigNumber.from(amount.toString())).send();
+        while(true){
+            try {
+                let a = await tronWeb.trx.getConfirmedTransaction(resp);
+                break;
+            } catch (e) {}
+        }
+        return resp;
+    }
 
     sum(a, b) {
         return a + b
