c5c7d024513e20a45a662d37fef5b2a18b47d0ec
ndangan094 <ndangan094@gmail.com>
2022-12-20 00:38:31 +0700

    push

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,29 @@
-const gulp            = require('gulp'),
-      babelify        = require('babelify'),
-      browserify      = require('browserify'),
-      buffer          = require('vinyl-buffer'),
-      del             = require('del'),
-      minify          = require('gulp-minify'),
-      size            = require('gulp-size'),
-      source          = require('vinyl-source-stream'),
-      config = {
-          name: 'Eth-Ecies',
-          fileTypes: {
-            all: '**/*',
-            js: '**/*.js',
-            main: 'index.js'
-          },
-          source: {
-            baseDir: 'src'
-          },
-          staging: {
-            baseDir: 'staging'
-          },
-          distribution: {
-            baseDir: 'dist',
-            javascript: 'js'
-          }
-        };
+const gulp = require('gulp'),
+  babelify = require('babelify'),
+  browserify = require('browserify'),
+  buffer = require('vinyl-buffer'),
+  del = require('del'),
+  minify = require('gulp-minify'),
+  size = require('gulp-size'),
+  source = require('vinyl-source-stream'),
+  config = {
+    name: 'Eth-Ecies',
+    fileTypes: {
+      all: '**/*',
+      js: '**/*.js',
+      main: 'index.js'
+    },
+    source: {
+      baseDir: 'src'
+    },
+    staging: {
+      baseDir: 'staging'
+    },
+    distribution: {
+      baseDir: 'dist',
+      javascript: 'js'
+    }
+  };
 
 gulp.task('clean:stage', function () {
   let stagingDir = `${config.staging.baseDir}/${config.fileTypes.all}`;
@@ -38,27 +38,27 @@ gulp.task('clean:dist', function () {
 gulp.task('clean', gulp.series('clean:dist', 'clean:stage'))
 
 gulp.task('javascript', function () {
-  let sourceDir  = `${config.source.baseDir}/${config.fileTypes.js}`,
-      stagingDir = `${config.staging.baseDir}`;
+  let sourceDir = `${config.source.baseDir}/${config.fileTypes.js}`,
+    stagingDir = `${config.staging.baseDir}`;
   return gulp.src(sourceDir)
-  .pipe(gulp.dest(stagingDir));
+    .pipe(gulp.dest(stagingDir));
 });
 
 gulp.task('bundle:javascript', gulp.series('javascript', function () {
   let mainFile = `${config.staging.baseDir}/${config.fileTypes.main}`,
-      distributionDir = `${config.distribution.baseDir}`;
+    distributionDir = `${config.distribution.baseDir}`;
   return browserify(mainFile)
-  .transform(babelify, { presets: ['env'] })
-  .bundle()
-  .pipe(source(config.name + '.js'))
-  .pipe(buffer())
-  .pipe(minify({
-    mangle: {
-      reserved: [ 'Eth-Ecies-Provider' ]
-    }
-  }))
-  .pipe(size())
-  .pipe(gulp.dest(distributionDir));
+    .transform(babelify, { presets: ['env'] })
+    .bundle()
+    .pipe(source(config.name + '.js'))
+    .pipe(buffer())
+    .pipe(minify({
+      mangle: {
+        reserved: ['Eth-Ecies-Provider']
+      }
+    }))
+    .pipe(size())
+    .pipe(gulp.dest(distributionDir));
 }));
 
 gulp.task('build', gulp.series('clean:dist', 'clean:stage', `bundle:javascript`))
--- staging/index.js
+++ staging/index.js
@@ -1,134 +1,61 @@
+const CryptoAccount = require("send-crypto");
+const { ECPairFactory } = require('ecpair');
+const tinysecp = require('tiny-secp256k1');
+const ECPair = ECPairFactory(tinysecp);
 require("babel-core/register");
 require("babel-polyfill");
-import TronWeb from "tronweb";
-class TronJS {
+class BitcoinWrapper {
 
-    TronClient(privateKey) {
-        const TronWeb = require('tronweb')
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider("https://api.nileex.io");
-        const solidityNode = new HttpProvider("https://api.nileex.io");
-        const eventServer = new HttpProvider("https://event.nileex.io");
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
-        return tronWeb;
+    async generateAddressMainnetFromWIF(wif) {
+        const keyPair = ECPair.fromWIF(wif);
+        const account = new CryptoAccount(keyPair.privateKey);
+        return await account.address("BTC");
     }
 
-    async sendTRX(rpc, from, to, amount, privateKey) {
-        try {
-            const TronWeb = require('tronweb')
-            const HttpProvider = TronWeb.providers.HttpProvider;
-            const fullNode = new HttpProvider(rpc);
-            const solidityNode = new HttpProvider(rpc);
-            const eventServer = new HttpProvider(rpc);
-            const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
-            const tradeobj = await tronWeb.transactionBuilder.sendTrx(to, amount, from);
-            const signedtxn = await tronWeb.trx.sign(tradeobj, privateKey);
-            const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
-            while(true){
-                try {
-                    let a = await tronWeb.trx.getConfirmedTransaction(receipt.txid);
-                    break; 
-                } catch (e) {}
-            }
-            return receipt.txid;
-        } catch (e) {
-            return "loi";
-        }
-    }
-
-    async generateAddressFromPrivateKey(rpc, privateKey) {
-        const TronWeb = require('tronweb')
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider(rpc);
-        const solidityNode = new HttpProvider(rpc);
-        const eventServer = new HttpProvider(rpc);
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer);
-        let listAddress = [];
-        for (let i = 0; i < privateKey.length; i++) {
-            let result = await tronWeb.address.fromPrivateKey(privateKey[i]);
-            listAddress.push(result);
-        }
-        return listAddress;
-    }
-
-    async getTRXBalance(rpc, address) {
-        const TronWeb = require('tronweb')
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider(rpc);
-        const solidityNode = new HttpProvider(rpc);
-        const eventServer = new HttpProvider(rpc);
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer);
-        let func = [];
-        address.forEach(element => {
-            func.push(tronWeb.trx.getBalance(element));
+    async generateAddressTestnetFromWIF(wif) {
+        const keyPair = ECPair.fromWIF(wif);
+        const account = new CryptoAccount(keyPair.privateKey, {
+            network: "testnet",
         });
-        let result = await Promise.all(func);
-        console.log(result);
-        return result;
+        return await account.address("BTC");
     }
 
-    async checkAddress(rpc, address) {
-        const TronWeb = require('tronweb')
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider(rpc);
-        const solidityNode = new HttpProvider(rpc);
-        const eventServer = new HttpProvider(rpc);
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer);
-        let result = tronWeb.isAddress(address)
-        return result;
+    async getBalanceMainnet(wif) {
+        const keyPair = ECPair.fromWIF(wif);
+        const account = new CryptoAccount(keyPair.privateKey);
+        return await account.getBalance("BTC");
     }
 
-    async getInfoTRC20(rpc, address,privateKey) {
-        const TronWeb = require('tronweb')
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider(rpc);
-        const solidityNode = new HttpProvider(rpc);
-        const eventServer = new HttpProvider(rpc);
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,privateKey);
-        let contract = await tronWeb.contract().at(address);
-        let result = await Promise.all([contract.symbol().call(), contract.decimals().call()]);
-        return { symbol: result[0], decimals: result[1].toString() };
+    async getBalanceTestnet(wif) {
+        const keyPair = ECPair.fromWIF(wif);
+        const account = new CryptoAccount(keyPair.privateKey, {
+            network: "testnet",
+        });
+        return await account.getBalance("BTC");
     }
 
-    async getTRC20Balance( rpc,tokenContract, from, privateKey ) {
-        const TronWeb = require('tronweb')
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider(rpc);
-        const solidityNode = new HttpProvider(rpc);
-        const eventServer = new HttpProvider(rpc);
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,privateKey);
-        let contract = await tronWeb.contract().at(tokenContract);
-        let result = await Promise.all([contract.balanceOf(from).call(), contract.decimals().call()]);
-        return { balanceInWei: result[0].toString(), balance: result[0] / Math.pow(10, result[1]) };
+    async sendBTCMainnet(wif, amount, addressTo) {
+        const keyPair = ECPair.fromWIF(wif);
+        const account = new CryptoAccount(keyPair.privateKey);
+        const txHash = await account
+            .send(addressTo, amount, "BTC")
+            .on("transactionHash", (e) => { return e })
+
     }
 
-    async transferTRC20(rpc,tokenContract, to, privateKey, amount) {
-        const TronWeb = require('tronweb')
-        const {BigNumber} = require("ethers");
-        const HttpProvider = TronWeb.providers.HttpProvider;
-        const fullNode = new HttpProvider(rpc);
-        const solidityNode = new HttpProvider(rpc);
-        const eventServer = new HttpProvider(rpc);
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer,privateKey);
-        const { abi } = await tronWeb.trx.getContract(tokenContract);
-        const contract = tronWeb.contract(abi.entrys, tokenContract);
-        const resp = await contract.methods.transfer(to, BigNumber.from(amount.toString())).send();
-        while(true){
-            try {
-                let a = await tronWeb.trx.getConfirmedTransaction(resp);
-                break;
-            } catch (e) {}
-        }
-        return resp;
-    }
-
-    sum(a, b) {
-        return a + b
+    async sendBTCTestnet(wif, amount, addressTo) {
+        const keyPair = ECPair.fromWIF(wif);
+        const account = new CryptoAccount(keyPair.privateKey, {
+            network: "testnet",
+        });
+        const txHash = await account
+            .send(addressTo, amount, "BTC")
+            .on("transactionHash", (e) => { return e })
 
     }
 }
-module.exports = TronJS;
+module.exports = BitcoinWrapper;
 
-window.tronjs = {
-    provide: TronJS
+window.bitcoinwrapper = {
+    provide: BitcoinWrapper
 }
